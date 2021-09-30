10dcf884d788ebd38d570684c1a8595558e42014
Lenovo <binyamin.cohen111@gmail.com>
2021-09-30 17:59:57 +0300

    new one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Find a prime number by his index at the list
-import math
+from math import sqrt
 
 
 def index(n):
@@ -18,7 +18,7 @@ def gen_prime_num():
     count = 2
     while True:
         isprime = True
-        for x in range(2, int(math.sqrt(count) + 1)):
+        for x in range(2, int(sqrt(count) + 1)):
             if count % x == 0:
                 isprime = False
                 break
