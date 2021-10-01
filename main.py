b06f89dd131b4498e6abe2e91de589b47414fad1
Lenovo <binyamin.cohen111@gmail.com>
2021-10-01 16:43:18 +0300

    new one

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ from math import sqrt
 
 
 def index(n):
-    count = 0
+    count_idx = 0
     prime_num_lst = []
     gen = gen_prime_num()
-    while count <= n:
+    while count_idx <= n:
         prime_num_lst.append(gen.__next__())
-        if (count == n):
+        if count_idx == n:
             return prime_num_lst.pop(n)
         else:
-            count += 1
+            count_idx += 1
 
 
 def gen_prime_num():
-    count = 2
+    res = 2
     while True:
-        isprime = True
-        for x in range(2, int(sqrt(count) + 1)):
-            if count % x == 0:
-                isprime = False
+        is_prime = True
+        for x in range(2, int(sqrt(res) + 1)):
+            if res % x == 0:
+                is_prime = False
                 break
-        if isprime:
-            yield count
-        count += 1
+        if is_prime:
+            yield res
+        res += 1
 
 
 if __name__ == '__main__':
-    print(index(5))
+    print(index(4))
